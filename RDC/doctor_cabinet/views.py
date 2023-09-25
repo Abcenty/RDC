9f4e1742d41b242c50226eca8b4c4ce81409c930
Andrey Kazackov <AndreyKazackoB@yandex.ru>
2023-09-25 14:26:04 +0300

    Some media

--- RDC/doctor_cabinet/views.py
+++ RDC/doctor_cabinet/views.py
@@ -24,10 +24,12 @@ def doctors_request(request):
     return render(request, 'doctor_cabinet/doctors_request.html', context)
 
 
+# ЗАГРУЖЕННЫЙ ФАЙЛ ОБНОВЛЯЕТСЯ ПРИ ПРОСМОТРЕ, НУЖНО ЧТОБЫ ОСТАВАЛСЯ ПРЕЖНИЙ
 def analysis_request(request):
     doctor = Doctors.objects.get(user=request.user.id)
     # !!!  ИСПРАВЬ ФИЛЬТРАЦИИ, ОНО МОЖЕТ ВЗЯТЬ ПОСЛЕДНИЙ ЗАГРУЖЕННЫЙ, НО НЕ ОБЯЗАТЕЛЬНО ТЕКУЩЕГО ПОЛЬЗОВАТЕЛЯ !!!
     # ИЛИ ВЗЯТЬ НЕСКОЛЬКО ЗАПРОСОВ
+    file = Report.objects.latest('id').file
     comment = Report.objects.latest('id').comment
     context={
         'requests': Requests.objects.filter(doctor=doctor, status = 8),
